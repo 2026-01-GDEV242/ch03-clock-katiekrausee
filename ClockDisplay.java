749471cc71f71b6d798b6000e25aa8b89993d54b
katiekrausee <katiekrause97@gmail.com>
2026-02-25 14:38:54 -0500

    Update clock display header comment for 12 hour internal

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,9 +1,13 @@
 
 /**
+ * 12 HOUR INTERNAL BRANCH
+ * 
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
- * midnight).
+ * American-style 12 hour clock. The clock shows hours and minutes. 
+ * 
+ * The internal range of the clock is 00:00 (midnight) to 11:59 
+ * In order to track AM or PM, we will need additional Fields and logic to
+ * handle to switch in meridian. 
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
